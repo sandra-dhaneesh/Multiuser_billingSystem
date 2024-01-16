bcb265186799ed7d2b1596a0c59cacdc3458c4f8
sandra-inmakes <sandrais1221@gmail.com>
2024-01-16 23:48:31 +0530

    dropdown

--- BillSoftwareapp/admin.py
+++ BillSoftwareapp/admin.py
@@ -4,3 +4,5 @@ from .models import *
 # Register your models here.
 admin.site.register(modules_list)
 admin.site.register(company)
+admin.site.register(purchasedebit)
+admin.site.register(purchasedebit1)
